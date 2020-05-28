bf618ecf867e72e47210c0b448724d59f73382c7
Sherry <sherryshi2001@gmail.com>
2020-05-28 04:56:07 -0400

    clean up script.js for better style

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,32 +1,3 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
-
 /**
  * Adds a random fact about myself!
  */
@@ -46,23 +17,21 @@ function genRandomFact() {
   factContainer.innerText = fact;
 }
 
+/**
+ * Implements Sticky Nav Bar
+ */
 document.addEventListener('DOMContentLoaded', function() {
-
-    //Sticky Nav Bar
-    window.onscroll = function() {stickyNav()};
-
-    var navbar = document.getElementById("nav");
-
-    var offsetPos = navbar.offsetTop;
-
-    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function stickyNav() {
+  window.onscroll = function() {stickyNav()};
+  var navbar = document.getElementById("nav");
+  var offsetPos = navbar.offsetTop;
+  
+  // Add the sticky class to nav when scroll, remove when not
+  function stickyNav() {
     if (window.pageYOffset >= offsetPos) {
-        navbar.classList.add("sticky")
+      navbar.classList.add("sticky")
     } else {
-        navbar.classList.remove("sticky");
+      navbar.classList.remove("sticky");
     }
-    }
-
+  }
 });
 
