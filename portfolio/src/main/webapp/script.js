199a8a5f5c252c2450ac175048e5006dbfc2a356
Sherry <sherryshi2001@gmail.com>
2020-05-28 23:37:03 -0400

    add logic so randomfact doesn't repeat previous fact

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,3 +1,5 @@
+let lastRandomIndex;
+
 /**
  * Adds a random fact about myself!
  */
@@ -9,12 +11,24 @@ function genRandomFact() {
     'I am 19 years old!',
   ];
 
-  // Pick a random greeting.
-  const fact = facts[Math.floor(Math.random() * facts.length)];
+  // Pick a random greeting, no repeat.
+  let randomIndex = randomNumGenerator(facts.length);
+  console.log(randomIndex);
+  lastRandomIndex = randomIndex;
+  let fact = facts[randomIndex];
 
   // Add it to the page.
   const factContainer = document.getElementById('fact-container');
   factContainer.innerText = fact;
+}
+
+/**
+ * Generates random number from 0 to parameter factsLength, no repeat
+ * @param factsLength
+ */
+function randomNumGenerator(factsLength) {
+  let randomIndex = Math.floor(Math.random() * factsLength);
+  return (randomIndex===lastRandomIndex)? randomNumGenerator(factsLength):randomIndex;
 }
 
 /**
