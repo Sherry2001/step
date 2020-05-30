90145489cf5148f966b0b65a39a0eecea512fd90
Sherry <sherryshi2001@gmail.com>
2020-05-29 22:49:49 -0400

    improve suggestion box response message, clean up code and fix some of Rica's comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,17 +1,16 @@
-
 let lastRandomIndex;
 
-/**
- * Adds a random fact about myself!
- */
-function genRandomFact() {
-  const facts = [
+const facts = [
     'I love spicy food, but I dislike eating peppers!',
     'I play the flute!',
     'I have been taking lots of walks during quarantine',
     'I am 19 years old!',
   ];
 
+/**
+ * Adds a random fact about myself!
+ */
+function genRandomFact() {
   let randomIndex = randomNumGenerator(facts.length);
   console.log(randomIndex);
   lastRandomIndex = randomIndex;
@@ -27,7 +26,8 @@ function genRandomFact() {
  */
 function randomNumGenerator(factsLength) {
   let randomIndex = Math.floor(Math.random() * factsLength);
-  return (randomIndex === lastRandomIndex)? randomNumGenerator(factsLength):randomIndex;
+  return (randomIndex === lastRandomIndex)? 
+      randomNumGenerator(factsLength):randomIndex;
 }
 
 /**
@@ -61,7 +61,7 @@ function closeForm() {
  */
 document.addEventListener('DOMContentLoaded', (event) => {
   const scriptURL = "https://script.google.com/macros/s/AKfycbygq04RYi-5qwb82bmfONkahtZAsrz0WkSoGfHLgHVkPWnnmSI/exec";
-  const form = document.forms['recommendation-form']
+  const form = document.forms['recommendation-form'];
 							
   form.addEventListener('submit', e => {
     e.preventDefault();
@@ -74,13 +74,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
  *Response to recommendation form
  */
   function respond(responseText) {
+    document.getElementById("form-response").innerHTML = responseText;
+    document.getElementById("form-response").style.visibility = "visible";
     setTimeout(() => {
-        document.getElementById("form-response").innerHTML = responseText;
-    }, 500);
+        document.getElementById("form-response").innerHTML = "";
+        document.getElementById("form-response").style.visibility = "hidden";
+    }, 5000);
     document.getElementById('recommendation').value = "";
     document.getElementById('comment').value = "";
   }
 })
-
-
-		
