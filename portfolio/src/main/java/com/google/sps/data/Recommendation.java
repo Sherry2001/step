3b2354c06256ae799de22cb1351ffb50abc793cc
Sherry <sherryshi2001@gmail.com>
2020-06-05 05:42:12 +0000

    tabbing fix for Recommadendation.java

--- portfolio/src/main/java/com/google/sps/data/Recommendation.java
+++ portfolio/src/main/java/com/google/sps/data/Recommendation.java
@@ -3,18 +3,18 @@
 package com.google.sps.data;
 
 public final class Recommendation {
-    private final long id;
-    private final String name;
-    private final String category;
-    private final String content;
-    private final String comment;
+  private final long id;
+  private final String name;
+  private final String category;
+  private final String content;
+  private final String comment;
 
-    public Recommendation(long id, String name, String category, String content,
+  public Recommendation(long id, String name, String category, String content,
                           String comment) {
-        this.id = id;
-        this.name = name;
-        this.category = category;
-        this.content = content;
-        this.comment = comment; 
-    } 
+    this.id = id;
+    this.name = name;
+    this.category = category;
+    this.content = content;
+    this.comment = comment; 
+  } 
 }
